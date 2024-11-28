3a2cebf588890378cb3ee9848cba11c2ca795205
RVC11 <membermadde.insiddev25120@hotmail.com>
2024-11-28 20:49:45 +0530

    Update caesarcipher.py

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,6 +1,17 @@
+"""
+This module provides a Caesar cipher encryption and decryption methods.
+"""
+
 symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 def encrypt(string, shift):
+    """
+    Encrypts the given string using a Caesar cipher with the given shift.
+
+    :param string: The string to be encrypted.
+    :param shift: The amount of characters to shift.
+    :return: The encrypted string.
+    """
     ciphered_text = ''
     shifted_symbols = symbols[shift:] + symbols[:shift]
 
@@ -13,6 +24,13 @@ def encrypt(string, shift):
     return ciphered_text
 
 def decrypt(string, shift):
+    """
+    Decrypts the given string using a Caesar cipher with the given shift.
+
+    :param string: The string to be decrypted.
+    :param shift: The amount of characters to shift.
+    :return: The decrypted string.
+    """
     ciphered_text = ''
     shifted_symbols = symbols[-shift:] + symbols[:-shift]
 
@@ -24,12 +42,12 @@ def decrypt(string, shift):
 
     return ciphered_text
 
+if __name__ == '__main__':
+    string = input("Enter a string: ")
+    mode = input("Select a mode (e to encrypt and d to decrypt): ")
+    shift = int(input("How much to shift? "))
 
-string = input("Enter a string: ")
-mode = input("Select a mode (e to encrypt and d to decrypt): ")
-shift = int(input("How much to shift? "))
-
-if mode == 'e':
-    print(encrypt(string, shift))
-elif mode == 'd':
-    print(decrypt(string, shift))
+    if mode == 'e':
+        print(encrypt(string, shift))
+    elif mode == 'd':
+        print(decrypt(string, shift))
